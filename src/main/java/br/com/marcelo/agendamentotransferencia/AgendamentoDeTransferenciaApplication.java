39f8ade8c481ca7931e3aefad9dce448e78874a9
esoft41 - Marcelo Banhos <marcelobanhoss@gmail.com>
2018-10-11 14:02:05 -0300

    commit inicial

---
+++ src/main/java/br/com/marcelo/agendamentotransferencia/AgendamentoDeTransferenciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcelo.agendamentotransferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoDeTransferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoDeTransferenciaApplication.class, args);
+	}
+
+}
